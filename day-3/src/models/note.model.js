267d889b64fd3045d041948ae1a3e66ee7a44f98
Het-Gajjar <hetg69774@gmail.com>
2026-02-04 19:53:19 +0530

    Delete day-3 directory

--- day-3/src/models/note.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-const noteSchema=new mongoose.Schema({
-    title:String,
-    description:String
-})
-
-const nodeModel=mongoose.model("notes",noteSchema)
-module.exports=nodeModel;
